d135ebd2bbbf450c14011aa8368b4a6305336e22
Xargin <buaa.cch@live.com>
2020-07-17 11:38:17 +0800

    80% done

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,27 @@
     <div class="card">
       <h2>Admin</h2>
       <h4>IP</h4>
-      <input type="text" :value="admin.ip" />
-      <br />
+      <p>
+        <input type="text" :value="admin.ip" />
+      </p>
       <h4>Port</h4>
       <input type="text" :value="admin.port" />
     </div>
     <div class="card">
       <h2>Tracing</h2>
       <h4>Enable</h4>
-      <input type="text" :value="tracing.enable" />
-      <br />
+      <p>
+        <input type="text" :value="tracing.enable" />
+      </p>
       <h4>Driver</h4>
       <input type="text" :value="tracing.driver" />
     </div>
     <div class="card">
       <h2>PProf</h2>
       <h4>Enable</h4>
-      <input type="text" :value="pprof.debug" />
-      <br />
+      <p>
+        <input type="text" :value="pprof.debug" />
+      </p>
       <h4>Port</h4>
       <input type="text" :value="pprof.port_value" />
     </div>
@@ -44,7 +47,12 @@
 
     <!-- cluster start -->
     <div class="card_wide">
-      <h2>Cluster</h2>
+      <h2>
+        Cluster
+        <a v-on:click="append_cluster" :id="index">add</a>
+        /
+        <a v-on:click="remove_cluster" :id="index">del</a>
+      </h2>
       <table v-for="(cluster,index) in clusters" :key="index" style="border:1px solid gray">
         <tr>
           <td>
@@ -70,15 +78,19 @@
         </tr>
         <tr>
           <td>
-            <h4>address list:</h4>
+            <h4>
+              address list:
+              <a v-on:click="add_addr_to_cluster" :id="index">add</a>
+              /
+              <a v-on:click="remove_addr_to_cluster" :id="index">del</a>
+            </h4>
             <input
               class="addr_box"
               type="text"
-              v-for="item in clusters.hosts"
+              v-for="item in cluster.hosts"
               :key="item.address"
               :value="item.address"
             />
-            <a v-on:click="add_addr_to_cluster">add</a>
           </td>
         </tr>
       </table>
@@ -87,7 +99,12 @@
 
     <!-- router start -->
     <div class="card_wide">
-      <h2>Router</h2>
+      <h2>
+        Router
+        <a v-on:click="append_router">add</a>
+        /
+        <a v-on:click="remove_router">del</a>
+      </h2>
       <table v-for="(router, index) in routers" :key="index" style="border:1px solid gray">
         <tr>
           <td>
@@ -95,7 +112,12 @@
             <input type="text" :value="router.router_config_name" />
           </td>
           <td>
-            <h4>routers</h4>
+            <h4>
+              routers
+              <a v-on:click="append_internal_router" :id="index">add</a>
+              /
+              <a v-on:click="remove_internal_router" :id="index">del</a>
+            </h4>
             <div v-for="(vhost, index) in router.virtual_hosts" :key="index" class="vhost_block">
               <label>host name</label>
               <input type="text" :value="vhost.name" />
@@ -117,7 +139,7 @@
     <!-- listener start -->
     <div class="card_wide">
       <h2>Listener</h2>
-      <div v-for="listener in listeners" :key="listener.name" style="border:1px solid gray">
+      <div v-for="(listener,i) in listeners" :key="listener.name" style="border:1px solid gray">
         <table>
           <tr>
             <td>
@@ -135,7 +157,19 @@
           </tr>
         </table>
         <table>
+          <th>
+            filter chain
+            <a v-on:click="append_filter_chain" :id="i">add</a>
+            /
+            <a v-on:click="remove_filter_chain" :id="i">del</a>
+          </th>
           <tr v-for="(chain, index) in listener.filter_chains" :key="index">
+            <h4>
+              chain {{index}}
+              <a v-on:click="append_filter" :id="i" :name="index">add filter</a>
+              /
+              <a v-on:click="remove_filter" :id="i" :name="index">del filter</a>
+            </h4>
             <div v-for="filter in chain.filter" :key="filter.index" style="border:1px solid gray">
               <td>
                 <h4>filter_type</h4>
@@ -243,12 +277,89 @@ export default {
     };
   },
   methods: {
+    //------- cluster start
     add_addr_to_cluster: function(event) {
-      this.clusters.hosts.push({ address: "127.0.0.1:8080" });
+      this.clusters[event.target.id].hosts.push({ address: "127.0.0.1:8080" });
     },
-    show_window: function(event) {
-      alert(event.target.name);
+    remove_addr_to_cluster: function(event) {
+      this.clusters[event.target.id].hosts.pop();
     },
+    append_cluster: function(event) {
+      this.clusters.push({ hosts: [{ address: "127.0.0.1:8080" }] });
+    },
+    remove_cluster: function(event) {
+      this.clusters.pop();
+    },
+    //------- cluster end
+    //------- router start
+    append_router: function(event) {
+      this.routers.push({
+        router_config_name: "serverRouter",
+        virtual_hosts: [
+          {
+            name: "serverHost",
+            domains: '["*"]',
+            routers: [
+              {
+                match: '{prefix : "/"}', // 还可能有其它结果
+                route: { cluster_name: "serverCluster" }
+              }
+            ]
+          }
+        ]
+      });
+    },
+    remove_router: function(event) {
+      this.routers.pop();
+    },
+    append_internal_router: function(event) {
+      this.routers[event.target.id].virtual_hosts.push({
+        name: "serverHost",
+        domains: '["*"]',
+        routers: [
+          {
+            match: '{prefix : "/"}', // 还可能有其它结果
+            route: { cluster_name: "serverCluster" }
+          }
+        ]
+      });
+    },
+    remove_internal_router: function(event) {
+      this.routers[event.target.id].virtual_hosts.pop();
+    },
+    //------- router end
+    //------- listener start
+    append_filter_chain: function(event) {
+      this.listeners[event.target.id].filter_chains.push({
+        filter: [
+          {
+            type: "proxy",
+            config: {
+              downstream_protocol: "Http1",
+              upstream_protocol: "Http1",
+              router_config_name: "serverRouter"
+            }
+          }
+        ]
+      });
+    },
+    append_filter: function(event) {
+      this.listeners[event.target.id].filter_chains[event.target.name].filter.push({
+        type: "proxy",
+        config: {
+          downstream_protocol: "Http1",
+          upstream_protocol: "Http1",
+          router_config_name: "serverRouter"
+        }
+      });
+    },
+    remove_filter: function(event) {
+      this.listeners[event.target.id].filter_chains[event.target.name].filter.pop();
+    },
+    remove_filter_chain: function(event) {
+      this.listeners[event.target.id].filter_chains.pop();
+    },
+    //------- listener end
     generate_json: function(event) {
       var x = {
         log: this.log,
@@ -306,7 +417,7 @@ h4 {
 
 .card {
   height: 160px;
-  padding: 10px;
+  padding: 15px;
   text-align: left;
   width: 22%;
   display: inline-block;
